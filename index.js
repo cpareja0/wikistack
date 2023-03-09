f4a528488359c3fe90f75dd467f0ab65c67574d3
Calvin Pareja <122660430+cpareja0@users.noreply.github.com>
2023-03-09 09:52:13 -0800

    setup server template

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname, '/public'));
-app.use(express.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-const PORT = 1337
+const PORT = 3000
 
-app.listen((PORT) =>{
+app.listen(PORT, () =>{
     console.log(`Listening on Port: ${PORT}`)
 })
