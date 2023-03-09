5b6779d41584d2ba8d2dad2cb20d6a4bb5741c53
Calvin Pareja <122660430+cpareja0@users.noreply.github.com>
2023-03-09 09:45:05 -0800

    init setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname, '/public'));
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+const PORT = 1337
+
+app.listen((PORT) =>{
+    console.log(`Listening on Port: ${PORT}`)
+})
