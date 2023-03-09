0712ac07946bb4dbfa2089e4a675434560fa4578
Jake Alagel <alagel.jacob@gmail.com>
2023-03-09 15:47:30 -0500

    added syncFunc

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const { db, Page, User } = require("./models");
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-const PORT = 3000
+const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Listening on Port: ${PORT}`)
-})
+const syncFunc = async () => {
+  await User.sync();
+  await Page.sync();
+
+  app.listen(PORT, () => {
+    console.log(`Listening on Port: ${PORT}`);
+  });
+};
+syncFunc();
